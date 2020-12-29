d206fe29dce6095ba52487d5890ecc7c3ab13dd7
Virginia Balseiro <virginiabalseiro@gmail.com>
2020-12-29 15:50:45 +0100

    Adding user authentication

---
+++ src/App.jsx
@@ -0,0 +1,49 @@
+import React from "react";
+import {
+  LoginButton,
+  LogoutButton,
+  Text,
+  useSession,
+  CombinedDataProvider,
+} from "@inrupt/solid-ui-react";
+import "./App.css";
+
+const authOptions = {
+  clientName: "Solid Todo App",
+};
+
+function App() {
+  const { session } = useSession();
+  return (
+    <div className="app-container">
+      {session.info.isLoggedIn ? (
+        <CombinedDataProvider
+          datasetUrl={session.info.webId}
+          thingUrl={session.info.webId}
+        >
+          <div className="message logged-in">
+            <span>You are logged in as: </span>
+            <Text
+              properties={[
+                "http://xmlns.com/foaf/0.1/name",
+                "http://www.w3.org/2006/vcard/ns#fn",
+              ]}
+            />
+            <LogoutButton />
+          </div>
+        </CombinedDataProvider>
+      ) : (
+        <div className="message">
+          <span>You are not logged in. </span>
+          <LoginButton
+            oidcIssuer="https://broker.pod.inrupt.com/"
+            redirectUrl={window.location.href}
+            authOptions={authOptions}
+          />
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from "react-dom";
+import App from "./App";
+import { SessionProvider } from "@inrupt/solid-ui-react";
 
 ReactDOM.render(
-  <React.StrictMode>
+  <SessionProvider>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </SessionProvider>,
+  document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
